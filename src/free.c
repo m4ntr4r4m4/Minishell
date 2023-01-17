f7c215eb58149cfe39a607c2482a36439e9a33d8
Abdelghani Hammoud <ahammoud@c3r14s1.42madrid.com>
2023-01-17 18:22:56 +0100

    signals done

--- src/free.c
+++ src/free.c
@@ -6,7 +6,7 @@
 /*   By: ahammoud <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 14:20:54 by ahammoud          #+#    #+#             */
-/*   Updated: 2023/01/05 17:19:53 by ahammoud         ###   ########.fr       */
+/*   Updated: 2023/01/17 14:45:24 by ahammoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	freetable(char **str)
 	i = -1;
 	while (str[++i])
 	{
-	//	fprintf(stderr,"********** FREE %s **********\n", str[i]);
+//		fprintf(stderr,"********** FREE %s **********\n", str[i]);
 		free(str[i]);
 	}
 	free(str[i]);
--- src/main.c
+++ src/main.c
@@ -2,42 +2,56 @@
 
 void	all_fill(t_all *all)
 {
+	strdup("./myls");
+
 	all->size = 1;
 	all->cmd = malloc(sizeof(*all->cmd) * 1);
-	all->cmd[0].path = get_path(all->path, "ls", X_OK);
+//	all->cmd[0].path = get_path(all->path, "ls", X_OK);
+//	all->cmd[0].path = strdup("/Users/ahammoud/github/Minishell/myls");
+	all->cmd[0].path = strdup("./executor");
 	fprintf(stderr, "thdhfgkhjk ************ %s\n", all->cmd[0].path);
-	all->cmd[0].name = strdup("ls");
+//	all->cmd[0].name = strdup("/Users/ahammoud/github/Minishell/myls");
+	all->cmd[0].name = strdup("./myls");
 	all->cmd[0].args = malloc(sizeof(*all->cmd[0].args) * 2);
+	all->cmd[0].token = malloc(sizeof(*all->cmd[0].token) * 1);
 	all->cmd[0].infile = malloc(sizeof(*all->cmd[0].infile) * 2);
 	all->cmd[0].outfile = malloc(sizeof(*all->cmd[0].outfile) * 2);
-	all->cmd[0].infile[0] = strdup("/bin/ls");
-	all->cmd[0].outfile[0] = strdup("/bin/ls");
-	all->cmd[0].args[0] = strdup("/bin/ls");
+	all->cmd[0].infile[0] = strdup("./myls");
+	all->cmd[0].infile[1] = NULL;
+	all->cmd[0].outfile[0] = strdup("./myls");
+	all->cmd[0].outfile[1] = NULL;
+//	all->cmd[0].args[0] = strdup("/Users/ahammoud/github/Minishell/myls");
+	all->cmd[0].args[0] = strdup("./executor");
+	all->cmd[0].args[1] = NULL;
 	all->cmd[0].n_tokens = 0;
 }
-int		xx = 0;
 
-void	intHandler(int x)
+void	Handlerint(int x)
 {
-	if (x == 2)
-	{	
-		xx =1;
-	}
-
+	ioctl(0, TIOCSTI, "\n");
+	rl_replace_line("", 0);
+	rl_on_new_line();
 }
-
+void	Handlerquit(int x)
+{
+	rl_on_new_line();
+}
 int	main(int ac,char **av,  char **envp)
 {
 	t_all	all;
 	char *rd;
 	char **new;
 
+	fprintf(stderr," this is a new shell\n");
 	all.exit_var = 0;
-	signal(SIGINT, intHandler);
+	signal(SIGINT, Handlerint);
+	signal(SIGQUIT, Handlerquit);
 	atexit(leaks);
 	while(1)
 	{
 		rd = readline("$_MINI_SHELL_$:");
+		if (!rd)
+			exit(0);
 		if(rd)
 		{
 			if (ft_strncmp(rd, "\0", 1))
@@ -46,10 +60,10 @@ int	main(int ac,char **av,  char **envp)
 				ft_env_init(envp, &all);
 	  		   	all.path = path_var(all.myenv);
 
-			//	all_fill(&all);
+		//		all_fill(&all);
 	      	/* printf("%s\n", rd); */
 	      	/* ft_print_table(new, 1); */
-		      	new = parser(rd, &all, all.myenv);
+		     	new = parser(rd, &all, all.myenv);
 	      	/* executer */
 	
 				if (new)
